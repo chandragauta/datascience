8267f9f1edaee6c20d9ff17f0da65717cdc940a4
chandragauta <moon0411134@gmail.com>
2025-01-22 23:25:09 +0545

    test to send email

--- scr1.py
+++ scr1.py
@@ -1,3 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-print("helll, scraping world")
+import smtplib
+import time
+while True:
+    re = requests.get('https://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html')
+    res = re.content
+    soup = BeautifulSoup(res,'html')
+    price = float(soup.find('p',class_='price_color').text[1:])
+    if price < 60:
+        smt = smtplib.SMTP('smtp.gmail.com',587)
+        smt.ehlo()
+        smt.starttls()
+        smt.login('moon0411134@gmail.com','cabhhotenhzkdaps')
+        smt.sendmail('moon0411134@gmail.com',
+                    'moon0411134@gmail.com',
+                    f"subject:price notifier\n\n hI PRICE IS DROP TO {price},buy it")
+    smt.quit()
+    time.sleep(24*60*60)
